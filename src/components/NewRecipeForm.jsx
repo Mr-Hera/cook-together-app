3b63127309d9a0a2262a5ed432f0368dadcf529d
Mr-Hera <randellhera@gmail.com>
2023-04-16 03:26:58 +0300

    Feat: added modal portal app level authentication & authorization check

--- src/components/NewRecipeForm.jsx
+++ src/components/NewRecipeForm.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NewRecipeForm = () => {
     // const [user, setUser] = useState(null);
+    const [recipeCategory, setRecipeCategory] = useState("")
     const [recipeTitle, setRecipeTitle] = useState("")
     const [ingredients, setIngredients] = useState("")
     const [instructions, setInstructions] = useState('')
@@ -20,6 +21,7 @@ const NewRecipeForm = () => {
         //     r.json().then((user) => setUser(user));
         //   }
         // });
+        setRecipeCategory("");
         setRecipeTitle("");
         setIngredients("");
         setInstructions("");
@@ -27,13 +29,25 @@ const NewRecipeForm = () => {
   return (
     <>
         <div className="flex flex-col justify-center my-4 p-8 w-3/4">
-            <div className="flex flex-col mt-40  justify-center align-center mx-auto w-3/4">
+            <div className="flex flex-col mt-28  justify-center align-center mx-auto w-3/4 bg-black">
                 <div className="border-b w-3/4 m-8 p-4">
                     <h4 className="text-center text-white font-semibold text-4xl damn">
                         👨🏾‍🍳CREATE RECIPE 👩🏾‍🍳
                     </h4>
                 </div>
-                <form onSubmit={handleSubmit} className="flex flex-col w-3/4 m-10 ">
+                <form onSubmit={handleSubmit} className="flex flex-col w-3/4 m-10">
+                    <div className="relative border-0 p-3 placeholder:text-2xl placeholder-current mb-3 px-2">
+                        <input
+                            id="category"
+                            name="category"
+                            type="text"
+                            className="peer bg-black text-2xl text-white border w-full p-4 placeholder-transparent transition duration-200"
+                            placeholder="Category"
+                            value={recipeCategory}
+                            onChange = { (e) => setRecipeCategory(e.target.value) }
+                        />
+                        <label htmlFor="username" className="absolute text-2xl font-semibold text-opacity-80 left-0 -top-2 bg-black text-white p-0.5 transition-all peer-placeholder-shown:top-7 peer-placeholder-shown:font-normal peer-placeholder-shown:bg-transparent mx-6 transition duration-200 input-text">Category</label>
+                    </div>
                     <div className="relative border-0 p-3 placeholder:text-2xl placeholder-current mb-3 px-2">
                         <input
                             id="title"
